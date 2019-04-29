3ae45af14cca67c51b7c968cb730c8c379905491
seinaas <assadian.seina@gmail.com>
2019-04-29 03:38:49 -0400

    Moved logged in template
    Create dedicated logged in template to use on both login and signup page.

---
+++ imports/ui/loggedin/logged-in.js
@@ -0,0 +1,14 @@
+import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+
+import './logged-in.html';
+
+if (Meteor.isClient) {
+    Template.loggedin.events({
+        'click .goToChat': function (event) {
+            if (Meteor.user()) {
+                FlowRouter.go('/');
+            }
+        },
+    });
+}
