0dee226ae2091c6be83a252c2f0c48f7f755fdd9
Seina Assadian <38442884+seinaas@users.noreply.github.com>
2020-05-25 19:41:51 -0400

    Update accounts-config.js

--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -2,7 +2,7 @@ import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 
 if (Meteor.isServer) {
   //MAIL URL from mailgun to send emails (sandbox for now)
-  process.env.MAIL_URL = "smtps://postmaster%40sandbox61d34bbb2c39493db05b974d93b23c5d.mailgun.org:a4e4a5a836ded2065e44bdd0065d06ba-16ffd509-7a42c361@smtp.mailgun.org:465";
+  process.env.MAIL_URL = "";
 
   //Login with google config (clientId and secret from dev page)
   ServiceConfiguration.configurations.upsert(
@@ -10,8 +10,8 @@ if (Meteor.isServer) {
     {
       $set: {
         loginStyle: "popup",
-        clientId: "973496230486-4567jn0mvq939k67pofj5mpki2nfruob.apps.googleusercontent.com", // See table below for correct property name!
-        secret: "nBAaB2nEVsE6NpuaHp-Briy9"
+        clientId: "", // See table below for correct property name!
+        secret: ""
       }
     }
   );
@@ -22,8 +22,8 @@ if (Meteor.isServer) {
     {
       $set: {
         loginStyle: "popup",
-        appId: "1324686194339020", // See table below for correct property name!
-        secret: "23950a709833f1f58fed719c5c05bf7d"
+        appId: "", // See table below for correct property name!
+        secret: ""
       }
     }
   );
